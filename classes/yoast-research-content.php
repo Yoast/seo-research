f3a759694e0efb2548f9e081343781549ea20287
Jimmy Comack <jimmy@yoast.com>
2017-12-21 17:25:56 +0100

    Renamed content to text

--- classes/yoast-research-content.php
+++ classes/yoast-research-content.php
@@ -17,18 +17,18 @@ class Yoast_Research_Content {
 	/**
 	 * @var string
 	 */
-	public $content;
+	public $text;
 
 	/**
 	 * Yoast_Research_Content constructor.
 	 *
 	 * @param string $title The title of the post or term.
-	 * @param string $url The URL of the post or term.
-	 * @param string $content The content of the post or term.
+	 * @param string $url   The URL of the post or term.
+	 * @param string $text  The text of the post or term.
 	 */
-	public function __construct( $title, $url, $content ) {
+	public function __construct( $title, $url, $text ) {
 		$this->title = $title;
-		$this->url = $url;
-		$this->content = $content;
+		$this->url   = $url;
+		$this->text  = $text;
 	}
 }
